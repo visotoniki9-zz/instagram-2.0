60e3b39da49ec0591c8a7f31f16e7e6ad5b55971
visotoniki9 <d.simon.ssx0@gmail.com>
2021-10-13 21:22:31 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+export default {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
